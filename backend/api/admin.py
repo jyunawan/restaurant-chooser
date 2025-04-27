ce6a5b48bff473a4c4e6a318224d9227b217dbeb
Jonathan Yunawan <jyunawan@gmail.com>
2025-04-27 01:52:49 -0700

    Created a UserCreationForm, UserChangeForm and UserAdmin for the custom User model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,86 @@
+from django import forms
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ["email", "display_name"]
+
+    def clean_password(self):
+        """Checks that the two passwords match"""
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password1
+
+    def save(self, commit=True):
+        """Saves the password in a hashed format"""
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    """
+    A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ["email", "display_name", "password", "is_active", "is_staff"]
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["email", "display_name", "is_staff"]
+    list_filter = ["is_staff"]
+    fieldsets = [
+        (None, {"fields": ["email", "display_name", "password"]}),
+        ("Permissions", {"fields": ["is_staff"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "display_name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
