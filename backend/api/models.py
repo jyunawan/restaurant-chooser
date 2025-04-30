040d923543f8d19406ac618fa041ee83cf4cf564
Jonathan Yunawan <jyunawan@gmail.com>
2025-04-30 02:31:46 -0700

    Created models for the session and restaurant suggestions.

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import (
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from .utils import generate_code
 
 
 class CustomUserManager(BaseUserManager):
@@ -124,3 +125,71 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         """Calls the normalize function on the email and display_name"""
         setattr(self, self.USERNAME_FIELD, self.normalize_email(self.get_username()))
         self.display_name = self.normalize_username(self.display_name)
+
+
+class Session(models.Model):
+    """
+    Sessions in this app are represented by this model
+
+    The creator field is required.
+    """
+
+    STAGE_CHOICES = {
+        "0": "Suggesting",
+        "1": "Banning",
+        "2": "Voting",
+        "3": "Results",
+    }
+
+    join_code = models.CharField(max_length=6, unique=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="session"
+    )
+    stage = models.CharField(max_length=1, choices=STAGE_CHOICES)
+
+    REQUIRED_FIELDS = ["creator"]
+
+    class Meta:
+        verbose_name = "session"
+        verbose_name_plural = "sessions"
+
+    def generate_unique_code(self):
+        """Uses the generate_code function to generate a unique code"""
+        code = generate_code()
+        while Session.objects.filter(code=code).exists():
+            code = generate_code()
+        return code
+
+    def save(self, *args, **kwargs):
+        """Modifies the save function to automatically generate a unique code"""
+        if not self.join_code:
+            self.join_code = self.generate_unique_code()
+        super().save(*args, **kwargs)
+
+
+class RestaurantSuggestion(models.Model):
+    """
+    Suggestions in this model are represented by this model.
+
+    The session the suggestion belongs to and the name of the restaurant are
+    required.
+    """
+
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, related_name="session"
+    )
+    name = models.CharField(max_length=100)
+    is_banned = models.BooleanField(default=False)
+    picks = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
+
+    REQUIRED_FIELDS = ["session", "name"]
+
+    class Meta:
+        verbose_name = "restaurant"
+        verbose_name_plural = "restaurants"
+
+    def __str__(self):
+        """Returns the restaurant's name"""
+        return self.display_name
