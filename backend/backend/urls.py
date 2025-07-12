d591a3839b764d7fc66a7ab1fc2d02aaa8d4b661
Jonathan Yunawan <jyunawan@gmail.com>
2025-07-12 12:37:41 -0700

    Created a new path for the session app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')), 
+    path('session/', include('session.urls')), 
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))] # catchall
