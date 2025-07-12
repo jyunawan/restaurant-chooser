0cd5c50bb3b4c517feb79f5f509e486a3a7fa36a
Jonathan Yunawan <jyunawan@gmail.com>
2025-07-12 12:37:57 -0700

    Changed the code generation to only include uppercase letters

--- backend/session/utils.py
+++ backend/session/utils.py
@@ -7,5 +7,5 @@ def generate_code():
     Returns a 5 digit code consisting of lowercase letters, uppercase letters, and digits
     """
     return "".join(
-        [random.choice(string.ascii_letters + string.digits) for _ in range(5)]
+        [random.choice(string.ascii_uppercase + string.digits) for _ in range(5)]
     )
